b94b429d9de78eadf2633f6613a734a75e9e3feb
robizona <robizona.info@gmail.com>
2026-09-13 02:33:48 +0200

    Checkpoint SDK e Herbs: menu, callback, blip e gruppi posizioni

---
+++ eateffects.lua
@@ -0,0 +1,133 @@
+-- File pubblico client: personalizza gli effetti per il tuo sistema di metabolismo.
+-- Nessun evento di rete viene registrato qui. Il runtime di consumo chiamerà la funzione
+-- HerbConsumptionEffects[plantId]() dopo aver verificato il consumo e il toggle della pianta.
+-- Le funzioni vuote non applicano effetti. Non inserire qui assegnazioni di premi server.
+HerbConsumptionEffects = {
+    ["agarita"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+        -- TriggerEvent("vorpmetabolism:changeValue", "Thirst", 1000)
+        -- TriggerEvent("vorpmetabolism:changeValue", "Hunger", 1000)
+        -- TriggerEvent('fred_meta:consume', 100, 100, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0) -- Fred Metabolism
+        
+    end,
+    ["alaskanginseng"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["americanginseng"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["baybolete"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["bitterweed"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["blackberry"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["blackcurrant"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["bloodflower"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["burdockroot"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["cardinalflower"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["chanterelle"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["chocolatedaisy"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["commonbulrush"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["creekplum"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["creepingthyme"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["crowsgarlic"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["desertsage"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["englishmace"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["evergreenhuckleberry"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["goldencurrant"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["harrietumofficinalis"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["hummingbirdsage"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["indiantobacco"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["milkweed"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["oleandersage"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["oregano"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["parasolmushroom"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["prairiepoppy"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["ramshead"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["redraspberry"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["redsage"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["saltbush"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["texasbluebonnet"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["violetsnowdrop"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["wildcarrot"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["wildfeverfew"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["wildmint"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["wildrhubarb"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["wintergreenberry"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["wisteria"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+    ["yarrow"] = function()
+        -- Inserisci qui la chiamata client al tuo sistema di effetti.
+    end,
+}
